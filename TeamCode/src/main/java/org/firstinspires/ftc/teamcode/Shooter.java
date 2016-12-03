9860bbb4c8d65765fc6c9479d80a1b931168f1cc
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2016-12-02 17:20:56 -0700

    Control shooter speed, ignore *~ files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+@TeleOp(name="Shooter Test", group="Test")
+//@Disabled
 public class Shooter extends LinearOpMode {
 
     DcMotor shooter1, shooter2;
@@ -12,15 +16,19 @@ public class Shooter extends LinearOpMode {
         shooter1 = hardwareMap.dcMotor.get("shooter1");
         shooter2 = hardwareMap.dcMotor.get("shooter2"); shooter2.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        shooter1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        shooter2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
         waitForStart();
 
-        double stick = gamepad1.right_stick_y;
+        while (opModeIsActive()) {
+            double stick = gamepad1.right_stick_y;
+            if (stick < 0) stick = 0;
 
-        if (stick < 0) stick = 0;
+            shooter1.setPower(stick * 0.24);
+            shooter2.setPower(stick * 0.24);
 
-        shooter1.setPower(stick);
-        shooter2.setPower(stick);
-
-        Thread.sleep(20);
+            Thread.sleep(20);
+        }
     }
 }
