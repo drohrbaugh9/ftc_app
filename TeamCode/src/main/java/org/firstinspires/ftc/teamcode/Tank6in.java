991aaf82f7a40e4f9d510fbc778be02b1c3c9274
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2017-02-04 17:07:20 -0700

    Add code to run Tank6in robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tank6in.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tank6in.java
@@ -1,8 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
-/**
- * Created by owner on 2/4/17.
- */
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-public class Tank6in {
+@TeleOp(name="Tank6in", group="Testing")
+@Disabled
+public class Tank6in extends OpMode {
+
+    DcMotor right, left;
+
+    public void init() {
+        right = hardwareMap.dcMotor.get("right");
+        left = hardwareMap.dcMotor.get("left");
+    }
+
+    public void loop() {
+        right.setPower(gamepad1.right_stick_y);
+        left.setPower(-gamepad1.left_stick_y);
+    }
 }
