f4f81d6baea0a622c75e69ff5eac7869480e4828
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2017-02-26 15:32:23 -0700

    Add LED_Test opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LED_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LED_Test.java
@@ -1,8 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+@TeleOp(name="LED_Test", group="Test")
+//@Disabled
 public class LED_Test extends LinearOpMode {
 
-    public void runOpMode() {}
+    DcMotor LED;
+
+    boolean LED_on, aPressed = false;
+
+    public void runOpMode() {
+
+        LED = hardwareMap.dcMotor.get("led");
+
+        LED.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        LED.setPower(0);
+
+        waitForStart();
+
+        if (gamepad1.a && !aPressed) {
+            if (!LED_on) LED.setPower(1);
+            else LED.setPower(0);
+            aPressed = true;
+        }
+        if (!gamepad1.a) aPressed = false;
+    }
 }
