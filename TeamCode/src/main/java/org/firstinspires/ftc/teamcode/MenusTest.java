036c2a62249842c611c0df44ade287e153409ca5
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2017-03-19 22:44:32 -0600

    Replace ArrayLists with Deques?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MenusTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MenusTest.java
@@ -1,19 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import java.util.ArrayList;
+import java.util.Deque;
 
 @TeleOp(name="MenusTest", group="Test")
 //@Disabled
 
 public class MenusTest extends OpMode {
 
-    ArrayList<String> COLORS;
-    ArrayList<String> PARTICLES;
-    ArrayList<String> PARKS;
+    Deque<String> COLORS;
+    Deque<String> PARTICLES;
+    Deque<String> PARKS;
     int DELAYS = 0; final int DELAY_MAX = 20;
 
     String[] menuItems = {"COLOR", "balls", " park", "delay"};
@@ -22,61 +21,58 @@ public class MenusTest extends OpMode {
     int activeItem = 0, ACTIVE_ITEM_MAX = 3;
 
     public void init() {
-        initializeArrays();
+        initializeDeques();
         displayMenu();
     }
 
     public void loop() {
         if (gamepad1.dpad_down) {
-            menuItems[activeItem].toLowerCase();
+            menuItems[activeItem] = menuItems[activeItem].toLowerCase();
             activeItem++;
             if (activeItem > ACTIVE_ITEM_MAX) activeItem = 0;
             if (activeItem < 0) activeItem = ACTIVE_ITEM_MAX;
-            menuItems[activeItem].toUpperCase();
+            menuItems[activeItem] = menuItems[activeItem].toUpperCase();
         } else if (gamepad1.dpad_up) {
-            menuItems[activeItem].toLowerCase();
+            menuItems[activeItem] = menuItems[activeItem].toLowerCase();
             activeItem--;
             if (activeItem < 0) activeItem = ACTIVE_ITEM_MAX;
             if (activeItem > ACTIVE_ITEM_MAX) activeItem = 0;
-            menuItems[activeItem].toUpperCase();
+            menuItems[activeItem] = menuItems[activeItem].toUpperCase();
         } else if (gamepad1.dpad_right) {
             if (activeItem == 3) {
                 DELAYS++;
                 if (DELAYS > DELAY_MAX) DELAYS = 0;
                 if (DELAYS < 0) DELAYS = DELAY_MAX;
             } else if (activeItem == 0) {
-                COLORS.add(COLORS.remove(0));
+                COLORS.addLast(COLORS.pollFirst());
             } else if (activeItem == 1) {
-                PARTICLES.add(PARTICLES.remove(0));
+                PARTICLES.addLast(PARTICLES.pollFirst());
             } else if (activeItem == 2) {
-                PARKS.add(PARTICLES.remove(0));
+                PARKS.addLast(PARTICLES.pollFirst());
             }
         } else if (gamepad1.dpad_left) {
             if (activeItem == 3) {
                 DELAYS--;
                 if (DELAYS < 0) DELAYS = DELAY_MAX;
                 if (DELAYS > DELAY_MAX) DELAYS = 0;
-            } else if (activeItem == 0) {
-                COLORS.add(0, COLORS.remove(COLORS.size() - 1));
-            } else if (activeItem == 1) {
-                PARTICLES.add(0, PARTICLES.remove(PARTICLES.size() - 1));
-            } else if (activeItem == 2) {
-                PARKS.add(0, PARKS.remove(PARKS.size() - 1));
             }
+            else if (activeItem == 0) COLORS.addFirst(COLORS.pollLast());
+            else if (activeItem == 1) PARTICLES.addFirst(PARTICLES.pollLast());
+            else if (activeItem == 2) PARKS.addFirst(PARKS.pollLast());
         }
 
         displayMenu();
     }
 
     private void displayMenu() {
-        telemetry.addData("color", menuItems[0] + S + COLORS.get(0));
-        telemetry.addData("balls", menuItems[1] + S + PARTICLES.get(0));
-        telemetry.addData("park", menuItems[2] + S + PARKS.get(0));
+        telemetry.addData("color", menuItems[0] + S + COLORS.peekFirst());
+        telemetry.addData("balls", menuItems[1] + S + PARTICLES.peekFirst());
+        telemetry.addData("park", menuItems[2] + S + PARKS.peekFirst());
         telemetry.addData("delay", menuItems[3] + S + DELAYS);
         telemetry.update();
     }
 
-    private void initializeArrays() {
+    private void initializeDeques() {
         COLORS.add("RED"); COLORS.add("BLUE");
 
         PARTICLES.add("1"); PARTICLES.add("2"); PARTICLES.add("3");
